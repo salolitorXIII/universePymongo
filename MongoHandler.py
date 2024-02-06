4c994a68b45b7b6e159e0b18832dfcd17cfbcf69
salolitorXIII <salolitor@alu.edu.gva.es>
2024-02-06 10:46:00 +0100

    6feb

--- MongoHandler.py
+++ MongoHandler.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pprint import pprint
@@ -7,12 +8,12 @@ class MongoHandler:
         self.client = MongoClient(uri)
         self.db = self.client['universo_db']
 
-    def imprimir_nombres_documento(self, coleccion, filtro = {}):
+    def imprimir_nombres_documento(self, coleccion, filtro):
         if filtro == {}:
             result = self.db[coleccion].find()
         else: 
             result = self.db[coleccion].find(filtro)
-            
+          
         if result == None:
             print('\nNo se han encontrado resultados')
         else:
@@ -32,12 +33,14 @@ class MongoHandler:
     def get_documento(self, coleccion, name):
         return self.db[coleccion].find_one({"name": name})
 
-    def existe_documento(self, coleccion, name, filtro = {}):
+    def existe_documento(self, coleccion, name, filtro):
         if filtro == {}:
             result = self.db[coleccion].find_one({"name": name})
         else:
-            result = self.db[coleccion].find_one({filtro})
-            
+            print(filtro)
+            result = self.db[coleccion].find_one({"$and": [{"name": name}, filtro]})
+            print(result)
+
         if result == None:
             print('\n¡¡¡ Opcion no valida !!!')
             return False
--- main.py
+++ main.py
@@ -13,34 +13,43 @@ db_handler = MongoHandler.MongoHandler("mongodb+srv://admin:admin@universbd.dxua
 
 # Menu principal
 def main_menu():
-    print('\n/=========================\\')
-    print('| (1) Mostrar galaxias    |')
-    print('| (2) Seleccionar galaxia |')
-    print('| (3) Añadir galaxia      |')
-    print('| (0) Salir               |')
-    print('\=========================/')
-    opcion = input('->> ')
+    while True:
+        print('\n/=========================\\')
+        print('| (1) Mostrar galaxias    |')
+        print('| (2) Seleccionar galaxia |')
+        print('| (3) Añadir galaxia      |')
+        print('| (0) Salir               |')
+        print('\=========================/')
+        opcion = input('->> ')
 
-    if opcion == '1':
-        db_handler.imprimir_nombres_documento("galaxies")
-    elif opcion == '2':
-        get_galaxia()
-    elif opcion == '3':
-        anadir_galaxia()
-    elif opcion == '0':
-        exit()
-    else:
-        print('\n¡¡¡ Opcion no valida !!!')
-        main_menu()
+        if opcion == '1':
+            db_handler.imprimir_nombres_documento("galaxies", {})
+        elif opcion == '2':
+            get_galaxia()
+        elif opcion == '3':
+            anadir_galaxia()
+        elif opcion == '0':
+            exit()
+        else:
+            print('\n¡¡¡ Opcion no valida !!!')
+
+
+
+
+
+
+
+
+
 
 # GET GALAXIA
 def get_galaxia():
-    db_handler.imprimir_nombres_documento("galaxies")
+    db_handler.imprimir_nombres_documento("galaxies", {})
     opcion = input('->> ')
-    if db_handler.existe_documento("galaxies", opcion):
+    if db_handler.existe_documento("galaxies", opcion, {}):
         galaxia(opcion)
     else:
-        main_menu()
+        print('\n¡¡¡ Galaxia no encontrada !!!')
 
 # CREATE GALAXIA
 def anadir_galaxia():
@@ -52,12 +61,10 @@ def anadir_galaxia():
         "num_planets": int(input('Numero de planetas->> ')),
     }
     db_handler.anadir_documento("galaxies", data)
-    main_menu()
 
 # READ GALAXIA
 def mostrar_galaxia(galaxia_obj: Galaxia):
     db_handler.imprimir_documento("galaxies", galaxia_obj._id)
-    galaxia(galaxia_obj.name)
 
 # UPDATE GALAXIA
 def modificar_galaxia(galaxia_obj: Galaxia):
@@ -68,65 +75,70 @@ def modificar_galaxia(galaxia_obj: Galaxia):
         "num_stars": int(input('Numero de estrellas->> ')),
         "num_planets": int(input('Numero de planetas->> ')),
     })
-    main_menu()
 
 # DELETE GALAXIA
 def eliminar_galaxia(galaxia_obj: Galaxia):
     db_handler.eliminar_documento("galaxies", galaxia_obj._id)
-    main_menu()
-
 
 # Menu galaxia
 def galaxia(galaxia_nombre):
-    galaxia_data = db_handler.get_documento("galaxies", galaxia_nombre)
-    galaxia_obj = Galaxia(
-        galaxia_data['_id'], 
-        galaxia_data['name'], 
-        galaxia_data['type'], 
-        galaxia_data['distance_ly'], 
-        galaxia_data['num_stars'], 
-        galaxia_data['num_planets']
-    )
+    while True:
+        galaxia_data = db_handler.get_documento("galaxies", galaxia_nombre)
+        galaxia_obj = Galaxia(
+            str(galaxia_data['_id']), 
+            galaxia_data['name'], 
+            galaxia_data['type'], 
+            galaxia_data['distance_ly'], 
+            galaxia_data['num_stars'], 
+            galaxia_data['num_planets']
+        )
 
-    print('\nGALAXIA: ' + galaxia_obj.name)
-    print('/=========================\\')
-    print('| (1) Mostrar sistemas    |')
-    print('| (2) Seleccionar sistema |')
-    print('| (3) Añadir sistema      |')
-    print('| (4) Mostrar galaxia     |')
-    print('| (5) Modificar galaxia   |')
-    print('| (6) Eliminar galaxia    |')
-    print('| (0) Atras               |')
-    print('\=========================/')
-    opcion = input('->> ')
+        print('\nGALAXIA: ' + galaxia_obj.name)
+        print('/=========================\\')
+        print('| (1) Mostrar sistemas    |')
+        print('| (2) Seleccionar sistema |')
+        print('| (3) Añadir sistema      |')
+        print('| (4) Mostrar galaxia     |')
+        print('| (5) Modificar galaxia   |')
+        print('| (6) Eliminar galaxia    |')
+        print('| (0) Atras               |')
+        print('\=========================/')
+        opcion = input('->> ')
 
-    if opcion == '1':
-        db_handler.imprimir_nombres_documento("sistemas_solar")
-    elif opcion == '2':
-        get_sistema(galaxia_obj)  
-    elif opcion == '3':
-        anadir_sistema(galaxia_obj)
-    elif opcion == '4':
-        mostrar_galaxia(galaxia_obj)
-    elif opcion == '5':
-        modificar_galaxia(galaxia_obj)
-    elif opcion == '6':
-        eliminar_galaxia(galaxia_obj)
-    elif opcion == '0':
-        main_menu()
-    else:
-        print('\n¡¡¡ Opcion no valida !!!')
-        galaxia(galaxia_nombre)
+        if opcion == '1':
+            db_handler.imprimir_nombres_documento("solar_systems", {"ref_galaxy": galaxia_obj._id})
+        elif opcion == '2':
+            get_sistema(galaxia_obj)  
+        elif opcion == '3':
+            anadir_sistema(galaxia_obj)
+        elif opcion == '4':
+            mostrar_galaxia(galaxia_obj)
+        elif opcion == '5':
+            modificar_galaxia(galaxia_obj)
+        elif opcion == '6':
+            eliminar_galaxia(galaxia_obj)
+        elif opcion == '0':
+            break
+        else:
+            print('\n¡¡¡ Opcion no valida !!!')
+
+
+
+
+
+
+
+
 
 
 # GET SISTEMA SOLAR
 def get_sistema(galaxia_obj: Galaxia):
     db_handler.imprimir_nombres_documento("solar_systems", {"ref_galaxy": galaxia_obj._id})
     opcion = input('->> ')
-    if db_handler.existe_documento("solar_systems", {"name": opcion, "ref_galaxy": galaxia_obj._id}):
+    if db_handler.existe_documento("solar_systems", opcion, {"ref_galaxy": galaxia_obj._id}):
         sistema_solar(opcion, galaxia_obj)
     else:
-        galaxia(galaxia_obj.name)
+        print('\n¡¡¡ Sistema solar no encontrado !!!')
 
 # CREATE SISTEMA SOLAR
 def anadir_sistema(galaxia_obj: Galaxia):
@@ -136,75 +148,308 @@ def anadir_sistema(galaxia_obj: Galaxia):
             "num_stars":{"$numberInt": input('Numero de estrellas->> ')},
             "ref_galaxy":galaxia_obj._id}
     db_handler.anadir_documento("solar_systems", data)
-    galaxia(galaxia_obj.name)
 
 # READ SISTEMA SOLAR
 def mostrar_sistema(sistema_obj: SistemaSolar):
     db_handler.imprimir_documento("solar_systems", sistema_obj._id)
-    sistema_solar(sistema_obj.name)
 
 # UPDATE SISTEMA SOLAR
-def modificar_sistema(sistema_obj: SistemaSolar, galaxia_obj: Galaxia):
+def modificar_sistema(sistema_obj: SistemaSolar):
     db_handler.modificar_documento("solar_systems", sistema_obj._id, {"name": input('Nombre->> '),
             "num_planets":{"$numberInt": input('Numero de planetas->> ')},
             "num_stars":{"$numberInt": input('Numero de estrellas->> ')}
             })
-    galaxia(galaxia_obj.name)
 
 # DELETE SISTEMA SOLAR
-def eliminar_sistema(galaxia_obj: Galaxia):
-    db_handler.eliminar_documento("galaxies", galaxia_obj._id)
-    galaxia(galaxia_obj.name)
-
+def eliminar_sistema(sistema_obj: SistemaSolar):
+    db_handler.eliminar_documento("solar_systems", sistema_obj._id)
 
 # Menu sistema
 def sistema_solar(sistema_nombre, galaxia_obj: Galaxia):
-    sistema_data = db_handler.get_documento("solar_systems", sistema_nombre)
-    sistema_obj = SistemaSolar(
-        sistema_data['_id'], 
-        sistema_data['name'], 
-        sistema_data['ref_galaxy']
-    )
+    while True:
+        sistema_data = db_handler.get_documento("solar_systems", sistema_nombre)
+        sistema_obj = SistemaSolar(
+            str(sistema_data['_id']), 
+            sistema_data['name'], 
+            sistema_data['ref_galaxy']
+        )
 
-    print('\nSISTEMA: ' + sistema_obj.name)
-    print('/=========================\\')
-    print('| (1) Mostrar estrellas   |')
-    print('| (2) Mostrar planetas    |')
-    print('| (3) Seleccionar estrella|')
-    print('| (4) Seleccionar planeta |')
-    print('| (5) Añadir estrella     |')
-    print('| (6) Añadir planeta      |')
-    print('| (7) Mostrar sistema     |')
-    print('| (8) Modificar sistema   |')
-    print('| (9) Eliminar sistema    |')
-    print('| (0) Atras               |')
-    print('\=========================/')
+        print('\nSISTEMA: ' + sistema_obj.name)
+        print('/=========================\\')
+        print('| (1) Mostrar estrellas   |')
+        print('| (2) Mostrar planetas    |')
+        print('| (3) Seleccionar estrella|')
+        print('| (4) Seleccionar planeta |')
+        print('| (5) Añadir estrella     |')
+        print('| (6) Añadir planeta      |')
+        print('| (7) Mostrar sistema     |')
+        print('| (8) Modificar sistema   |')
+        print('| (9) Eliminar sistema    |')
+        print('| (0) Atras               |')
+        print('\=========================/')
 
+        opcion = input('->> ')
+
+        if opcion == '1':
+            db_handler.imprimir_nombres_documento("stars", {"ref_solar_system": sistema_obj._id})
+        elif opcion == '2':
+            db_handler.imprimir_nombres_documento("planets", {"ref_solar_system": sistema_obj._id})
+        elif opcion == '3':
+            get_estrella(sistema_obj)
+        elif opcion == '4':
+            get_planeta(sistema_obj)
+        elif opcion == '5':
+            anadir_estrella(sistema_obj)
+        elif opcion == '6':
+            anadir_planeta(sistema_obj)
+        elif opcion == '7':
+            mostrar_sistema(sistema_obj)
+        elif opcion == '8':
+            modificar_sistema(sistema_obj)
+        elif opcion == '9':
+            eliminar_sistema(sistema_obj)
+        elif opcion == '0':
+            break
+        else:
+            print('\n¡¡¡ Opcion no valida !!!')
+
+
+
+
+
+
+
+
+
+
+# GET ESTRELLA
+def get_estrella(sistema_obj: SistemaSolar):
+    db_handler.imprimir_nombres_documento("stars", {"ref_solar_system": sistema_obj._id})
     opcion = input('->> ')
-
-    if opcion == '1':
-        db_handler.imprimir_nombres_documento("sistemas_solar")
-    elif opcion == '2':
-        get_sistema(galaxia_obj)  
-    elif opcion == '3':
-        pass
-    elif opcion == '4':
-        mostrar_galaxia(galaxia_obj)
-    elif opcion == '5':
-        modificar_galaxia(galaxia_obj)
-    elif opcion == '6':
-        eliminar_galaxia(galaxia_obj)
-    elif opcion == '7':
-        mostrar_sistema(sistema_obj)
-    elif opcion == '8':
-        modificar_sistema(sistema_obj)
-    elif opcion == '9':
-        eliminar_sistema(sistema_obj)
-    elif opcion == '0':
-        main_menu()
+    if db_handler.existe_documento("stars", opcion, {"ref_solar_system": sistema_obj._id}):
+        estrella(opcion, sistema_obj)
     else:
-        print('\n¡¡¡ Opcion no valida !!!')
-        sistema_solar(sistema_obj.name, galaxia_obj)
+        print('\n¡¡¡ Estrella no encontrada !!!')
+
+# CREATE ESTRELLA
+def anadir_estrella(sistema_obj: SistemaSolar):
+    data = {"name": input('Nombre->> '),
+            "type": input('Tipo->> '),
+            "mass_kg": input('Masa->> '),
+            "radius_km": input('Radio->> '),
+            "temperature_kelvin": input('Temperatura->> '),
+            "solar_system": sistema_obj.name,
+            "ref_solar_system": sistema_obj._id
+        }
+    db_handler.anadir_documento("stars", data)
+    
+# READ ESTRELLA
+def mostrar_estrella(estrella_obj: Estrella):
+    db_handler.imprimir_documento("stars", estrella_obj._id)
+
+# UPDATE ESTRELLA
+def modificar_estrella(estrella_obj: Estrella):
+    db_handler.modificar_documento("stars", estrella_obj._id, {"name": input('Nombre->> '),
+            "type": input('Tipo->> '),
+            "mass_kg": input('Masa->> '),
+            "radius_km": input('Radio->> '),
+            "temperature_kelvin": input('Temperatura->> ')
+        })
+
+# DELETE ESTRELLA
+def eliminar_estrella(estrella_obj: Estrella):
+    db_handler.eliminar_documento("stars", estrella_obj._id)
+
+# Menu estrella
+def estrella(estrella_nombre, sistema_obj: SistemaSolar):
+    while True:
+        estrella_data = db_handler.get_documento("stars", estrella_nombre)
+        estrella_obj = Estrella(
+            str(estrella_data['_id']), 
+            estrella_data['name'], 
+            estrella_data['ref_solar_system']
+        )
+
+        print('\nESTRELLA: ' + estrella_obj.name)
+        print('/=========================\\')
+        print('| (1) Mostrar estrella     |')
+        print('| (2) Modificar estrella   |')
+        print('| (3) Eliminar estrella    |')
+        print('| (0) Atras                |')
+        print('\=========================/')
+
+        opcion = input('->> ')
+
+        if opcion == '1':
+            mostrar_estrella(estrella_obj)
+        elif opcion == '2':
+            modificar_estrella(estrella_obj)
+        elif opcion == '3':
+            eliminar_estrella(estrella_obj)
+        elif opcion == '0':
+            break
+        else:
+            print('\n¡¡¡ Opcion no valida !!!')
+
+
+
+
+
+
+
+
+
+
+# GET PLANETA
+def get_planeta(sistema_obj: SistemaSolar):
+    db_handler.imprimir_nombres_documento("planets", {"ref_solar_system": sistema_obj._id})
+    opcion = input('->> ')
+    if db_handler.existe_documento("planets", opcion, {"ref_solar_system": sistema_obj._id}):
+        planeta(opcion, sistema_obj)
+    else:
+        print('\n¡¡¡ Planeta no encontrado !!!')
+
+# CREATE PLANETA
+def anadir_planeta(sistema_obj: SistemaSolar):
+    data = {"name": input('Nombre->> '),
+            "type": input('Tipo->> '),
+            "diameter_km": input('Diametro->> '),
+            "mass_kg": input('Masa->> '),
+            "moons": input('Lunas->> '),
+            "atmosphere": input('Atmosfera->> '),
+            "orbital_period_days": input('Periodo orbital->> '),
+            "temperature_avg_celsius": input('Temperatura media->> '),
+            "solar_system": sistema_obj.name,
+            "ref_solar_system": sistema_obj._id}
+    db_handler.anadir_documento("planets", data)
+    
+# READ PLANETA
+def mostrar_planeta(planeta_obj: Planeta):
+    db_handler.imprimir_documento("planets", planeta_obj._id)
+
+# UPDATE PLANETA
+def modificar_planeta(planeta_obj: Planeta):
+    db_handler.modificar_documento("planets", planeta_obj._id, {"name": input('Nombre->> '),
+            "type": input('Tipo->> '),
+            "diameter_km": input('Diametro->> '),
+            "mass_kg": input('Masa->> '),
+            "moons": input('Lunas->> '),
+            "atmosphere": input('Atmosfera->> '),
+            "orbital_period_days": input('Periodo orbital->> '),
+            "temperature_avg_celsius": input('Temperatura media->> ')
+        })
+
+# DELETE PLANETA
+def eliminar_planeta(estrella_obj: Estrella):
+    db_handler.eliminar_documento("stars", estrella_obj._id)
+
+# Menu planetas
+def planeta(planeta_nombre, sistema_obj: SistemaSolar):
+    while True:
+        planeta_data = db_handler.get_documento("stars", planeta_nombre)
+        planeta_obj = Estrella(
+            str(planeta_data['_id']), 
+            planeta_data['name'], 
+            planeta_data['ref_solar_system']
+        )
+
+        print('\nPLANETA: ' + planeta_obj.name)
+        print('/=========================\\')
+        print('| (1) Mostrar lunas       |')
+        print('| (2) Seleccionar luna    |')
+        print('| (3) Añadir luna         |')
+        print('| (4) Mostrar planeta     |')
+        print('| (5) Modificar planeta   |')
+        print('| (6) Eliminar planeta    |')
+        print('| (0) Atras               |')
+        print('\=========================/')
+        opcion = input('->> ')
+
+        if opcion == '1':
+            db_handler.imprimir_nombres_documento("stars", {"ref_solar_system": sistema_obj._id})
+        elif opcion == '2':
+            pass
+        elif opcion == '3':
+            pass
+        elif opcion == '4':
+            mostrar_planeta(planeta_obj)
+        elif opcion == '5':
+            modificar_planeta(planeta_obj)
+        elif opcion == '6':
+            eliminar_planeta(planeta_obj)
+        elif opcion == '0':
+            break
+        else:
+            print('\n¡¡¡ Opcion no valida !!!')
+
+
+
+
+
+
+
+
+
+
+# GET LUNA
+def get_luna(sistema_obj: SistemaSolar):
+    db_handler.imprimir_nombres_documento("stars", {"ref_solar_system": sistema_obj._id})
+    opcion = input('->> ')
+    if db_handler.existe_documento("stars", opcion, {"ref_solar_system": sistema_obj._id}):
+        estrella(opcion, sistema_obj)
+    else:
+        print('\n¡¡¡ Estrella no encontrada !!!')
+
+# CREATE LUNA
+def anadir_luna(sistema_obj: SistemaSolar):
+    data = {"name": input('Nombre->> '),
+            "ref_solar_system":sistema_obj._id}
+    db_handler.anadir_documento("stars", data)
+    
+# READ LUNA
+def mostrar_luna(estrella_obj: Estrella):
+    db_handler.imprimir_documento("stars", estrella_obj._id)
+
+# UPDATE LUNA
+def modificar_luna(estrella_obj: Estrella):
+    db_handler.modificar_documento("stars", estrella_obj._id, {"name": input('Nombre->> ')})
+
+# DELETE LUNA
+def eliminar_luna(estrella_obj: Estrella):
+    db_handler.eliminar_documento("stars", estrella_obj._id)
+
+# Menu luna
+def lunas(luna_nombre, planeta_obj: Planeta):
+    while True:
+        luna_data = db_handler.get_documento("stars", luna_nombre)
+        luna_obj = Estrella(
+            str(luna_data['_id']), 
+            luna_data['name'], 
+            luna_data['ref_solar_system']
+        )
+
+        print('\nLUNA: ' + luna_obj.name)
+        print('/=========================\\')
+        print('| (1) Mostrar luna        |')
+        print('| (2) Modificar luna      |')
+        print('| (3) Eliminar luna       |')
+        print('| (0) Atras               |')
+        print('\=========================/')
+
+        opcion = input('->> ')
+
+        if opcion == '1':
+            mostrar_luna(luna_obj)
+        elif opcion == '2':
+            modificar_luna(luna_obj)
+        elif opcion == '3':
+            eliminar_luna(luna_obj)
+        elif opcion == '0':
+            break
+        else:
+            print('\n¡¡¡ Opcion no valida !!!')
+
+
 
 
 
